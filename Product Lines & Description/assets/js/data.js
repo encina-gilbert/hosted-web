de661bffac7cc9e6f748f361ad8b3ef76974eb56
Gilbert Encina <48022361+encina-gilbert@users.noreply.github.com>
2026-01-23 06:51:08 +0200

    Fix quotes in audi_key object in data.js

--- Product Lines & Description/assets/js/data.js
+++ Product Lines & Description/assets/js/data.js
@@ -109,13 +109,13 @@ const stationsData = {
   },
   // Audi Key
   audi_key: {
-    "flash / pretest 1": [BBS13030_L01S01],
-    "flash / pretest 2": [BBS13030_L01S02],
-    eol_uhf_1: [BBS13200_L01S01],
-    eol_uhf_2: [BBS13200_L01S02],
-    eol_uhf_3: [BBS13200_L01S03],
-    eol_uwb1: [BBS13210_L01S01],
-    eol_uwb2: [BBS13210_L01S02],
+    "flash / pretest 1": ["BBS13030_L01S01"],
+    "flash / pretest 2": ["BBS13030_L01S02"],
+    eol_uhf_1: ["BBS13200_L01S01"],
+    eol_uhf_2: ["BBS13200_L01S02"],
+    eol_uhf_3: ["BBS13200_L01S03"],
+    eol_uwb1: ["BBS13210_L01S01"],
+    eol_uwb2: ["BBS13210_L01S02"],
   },
   // ZKW
   zkw: {
